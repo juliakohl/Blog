c5e51619527b3c8a0f9d06ef13d47d4c47a8f34f
Julia Kohl <julia.helmine.kohl@gmail.com>
2020-04-18 19:55:55 +0200

    set up pages

--- app.js
+++ app.js
@@ -15,18 +15,26 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function (req, res) {
+  res.render("home", {homeContent: homeStartingContent});
+});
 
+app.get("/about", function (req, res) {
+  res.render("about", {about: aboutContent});
+});
 
+app.get("/contact", function (req, res) {
+  res.render("contact", {contact: contactContent});
+});
 
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
 
-
-
-
-
-
-
-
-
+app.post("/compose", function (req,res) {
+  newPost = req.body.compose;
+  res.send("gepostet");
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
