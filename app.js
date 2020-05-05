5000df6ae3da4dd5fffd73bb1eccba75583ce9be
Julia Kohl <julia.helmine.kohl@gmail.com>
2020-05-06 00:24:23 +0200

    add sqlite3 and include db at compose, post and home page

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const sqlite3 = require('sqlite3').verbose();
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-let posts = [];
+//db.run('CREATE TABLE posts(title text, body text)');
 
 const app = express();
 
@@ -19,7 +17,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", {postList: posts});
+  let db = new sqlite3.Database('db/blog.db');
+  let sql = 'SELECT title, body FROM posts';
+  let posts = [];
+  let post = {};
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      post = {title: row.title, body: row.body};
+      posts.push(post);
+    });
+    console.log(posts);
+    db.close();
+    res.render("home", {postList: posts});  
+  });
 });
 
 app.get("/about", function (req, res) {
@@ -38,7 +52,22 @@ app.get("/posts/:title", function(req,res){
   let match = false;
   const requestedTitle = _.lowerCase(req.params.title);
   let output = {};
-  posts.forEach(function (post) {
+  let db = new sqlite3.Database('db/blog.db');
+  let sql = 'SELECT title, body FROM posts';
+  let posts = [];
+  let post = {};
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      post = {title: row.title, body: row.body};
+      posts.push(post);
+    });
+    db.close();
+
+    posts.forEach(function (post) {
     const postTitle = _.lowerCase(post.title)
     if(postTitle === requestedTitle){
       match = true;
@@ -49,16 +78,26 @@ app.get("/posts/:title", function(req,res){
     res.render("post", {title: output.title, text: output.body});
   } else {
     res.send("Ooops! Page not Found!")
-  }
+  }});
 });
 
 app.post("/compose", function (req,res) {
-  const post = { 
-    title: req.body.postTitle,
-    body: req.body.postBody
-  }
-  posts.push(post);
-  res.redirect("/");
+  let db = new sqlite3.Database('db/blog.db');
+  let title = req.body.postTitle;
+  let body = req.body.postBody;
+
+  db.run('INSERT INTO posts VALUES(?, ?)', [title, body],function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+    // get the last insert id
+    console.log(`A row has been inserted with rowid ${this.lastID}`);
+
+    if (!err){
+      db.close();
+      res.redirect("/");
+    }
+  });  
 });
 
 app.listen(3000, function() {
